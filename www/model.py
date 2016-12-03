ccb28a49f56d02d0608ef97c67eadc05a2079af7
wjiang14 <jwjovi@gmail.com>
2016-12-02 23:25:31 -0500

    finish model.py comment class

--- www/model.py
+++ www/model.py
@@ -34,3 +34,21 @@ class User(Model):
 class Blog(Model):
     __table__ = 'blogs'
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl="(varchar(50))")
+    user_name = StringField(ddl="varchar(50)")
+    user_image = StringField(ddl="varchar(500)")
+    name = StringField(ddl="valchar(50)")
+    summary = StringField(ddl="varchar(200)")
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+
+class Comment(Model):
+    __table__ = "comments"
+    id = StringField(primary_key=True, default=next_id, ddl="varchar(50)")
+    blog_id = StringField(ddl="varchar(50)")
+    user_id = StringField(ddl="varchar(50)")
+    user_name = StringField(ddl="varchar(500)")
+    user_image = StringField(ddl="varchar(500)")
+    content = TextField()
+    created_at = FloatField(default=time.time)
