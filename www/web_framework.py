ceea954ae797830220fa4f3aa939c7117857a554
wjiang14 <jwjovi@gmail.com>
2016-12-03 21:41:07 -0500

    add web_framework.py

---
+++ www/web_framework.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio, os, inspect, functools
+from urllib import parse
+from aiohttp import web
+from apis import APIError
+
+__author__ = 'Wei'
