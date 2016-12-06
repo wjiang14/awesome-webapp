0398beea5a3d62b335ebfcff42c584419ff09d18
wjiang14 <jwjovi@gmail.com>
2016-12-06 16:04:36 -0500

    inital mysql

--- www/schema.sql
+++ www/schema.sql
@@ -6,42 +6,42 @@ create database awesome;
 
 use awesome;
 
--- grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
+grant select, insert, update, delete on awesome.* to "root"@"localhost" identified by "root";
 
-create table users (
+create table users(
     `id` varchar(50) not null,
     `email` varchar(50) not null,
     `passwd` varchar(50) not null,
     `admin` bool not null,
     `name` varchar(50) not null,
     `image` varchar(500) not null,
-    `created_at` real not null,
-    unique key `idx_email` (`email`),  -- unique表示这是一个唯一约束列
-    key `idx_created_at` (`created_at`),  -- key表示索引，用来加快查询速度的
+    `created_at` real  not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
     primary key (`id`)
 ) engine=innodb default charset=utf8;
 
-create table blogs (
+create table blogs(
     `id` varchar(50) not null,
     `user_id` varchar(50) not null,
     `user_name` varchar(50) not null,
     `user_image` varchar(500) not null,
     `name` varchar(50) not null,
-    `summary` varchar(200) not null,
+    `summary` varchar(50) not null,
     `content` mediumtext not null,
-    `created_at` real not null,
+    `created_at` real  not null,
     key `idx_created_at` (`created_at`),
     primary key (`id`)
 ) engine=innodb default charset=utf8;
 
-create table comments (
+create table comments(
     `id` varchar(50) not null,
     `blog_id` varchar(50) not null,
     `user_id` varchar(50) not null,
     `user_name` varchar(50) not null,
     `user_image` varchar(500) not null,
     `content` mediumtext not null,
-    `created_at` real not null,
+    `created_at` real  not null,
     key `idx_created_at` (`created_at`),
     primary key (`id`)
 ) engine=innodb default charset=utf8;
